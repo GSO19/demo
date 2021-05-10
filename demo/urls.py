448cc1da394a3506191261185afb2c16f5691938
GSO19 <gso.globaliasoft1@gmail.com>
2021-05-10 17:15:19 +0530

    change in url

--- demo/urls.py
+++ demo/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # path('', views.index),
     path('emp', views.emp),  
-    path('',views.show),  
+    path('show',views.show),  
     path('edit/<int:id>', views.edit),  
     path('update/<int:id>', views.update),  
     path('delete/<int:id>', views.destroy),  
